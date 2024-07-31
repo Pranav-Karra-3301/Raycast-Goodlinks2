8df5f538fb7bde90ccd70920128af786fef4351d
Pranav Karra <pranavkarra001@gmail.com>
2024-08-01 00:12:46 +0530

    Initial Commit

---
+++ raycast-env.d.ts
@@ -0,0 +1,29 @@
+/// <reference types="@raycast/api">
+
+/* 🚧 🚧 🚧
+ * This file is auto-generated from the extension's manifest.
+ * Do not modify manually. Instead, update the `package.json` file.
+ * 🚧 🚧 🚧 */
+
+/* eslint-disable @typescript-eslint/ban-types */
+
+type ExtensionPreferences = {}
+
+/** Preferences accessible in all the extension's commands */
+declare type Preferences = ExtensionPreferences
+
+declare namespace Preferences {
+  /** Preferences accessible in the `add-link-from-clipboard` command */
+  export type AddLinkFromClipboard = ExtensionPreferences & {}
+  /** Preferences accessible in the `browse-links` command */
+  export type BrowseLinks = ExtensionPreferences & {}
+}
+
+declare namespace Arguments {
+  /** Arguments passed to the `add-link-from-clipboard` command */
+  export type AddLinkFromClipboard = {}
+  /** Arguments passed to the `browse-links` command */
+  export type BrowseLinks = {}
+}
+
+
